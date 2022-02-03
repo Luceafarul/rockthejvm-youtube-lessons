fb98800dd65aef07071b4fe7b3abba3dc4d8fd6b
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2022-02-03 21:45:24 +0200

    Update package structure again - remove multiprojects.

--- build.sbt
+++ build.sbt
@@ -2,14 +2,6 @@ import Dependencies._
 
 ThisBuild / scalaVersion := "2.13.8"
 
-lazy val `cats-lessons` = (project in file("cats-lessons")).settings(
-  libraryDependencies ++= Seq(
-    scalaTest % Test
-  )
-)
-
-lazy val tips = (project in file("tips")).settings(
-  libraryDependencies ++= Seq(
-    scalaTest % Test
-  )
+libraryDependencies ++= Seq(
+  scalaTest % Test
 )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,14 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9"
-  lazy val catsCore = "org.typelevel" %% "cats-core" % "2.3.0"
+  lazy val catsCore  = "org.typelevel" %% "cats-core" % "2.3.0"
+
+  val akkaVersion     = "2.6.8"
+  val akkaHttpVersion = "10.2.7"
+
+  // akka streams
+  lazy val akkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
+  // akka http
+  lazy val akkaHttp          = "com.typesafe.akka" %% "akka-http"            % akkaHttpVersion
+  lazy val akkaHttpSprayJson = "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion
 }
